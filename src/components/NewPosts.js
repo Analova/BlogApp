63a0ea426128751e4b7e6ec1e382d44674015f9b
Ana Nikolova <ani.nik19@gmail.com>
2019-09-30 09:20:12 +0200

    conditinal styling the form fields and added navigation to the cancel button

--- src/components/NewPosts.js
+++ src/components/NewPosts.js
@@ -1,13 +1,18 @@
 import React, { Component } from "react";
 import { Field, reduxForm } from "redux-form";
+import { Link } from "react-router-dom";
 
 class NewPosts extends Component {
   renderField(field) {
+    const {
+      meta: { touched, error }
+    } = field;
+    const className = `form-group ${touched && error ? "has-danger" : ""}`;
     return (
-      <div className="form-group">
+      <div className={className}>
         <label>{field.label}</label>
         <input className="form-control" type="text" {...field.input} />
-        {field.meta.touched ? field.meta.error : ""}
+        <div className="text-help">{touched ? error : ""}</div>
       </div>
     );
   }
@@ -34,6 +39,9 @@ class NewPosts extends Component {
         <button type="submit" className="btn btn-primary">
           Submit
         </button>
+        <Link to="/" className="btn btn-danger">
+          Cancel
+        </Link>
       </form>
     );
   }
