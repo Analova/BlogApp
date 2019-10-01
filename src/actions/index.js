1e051b5be115aa34a32e6bfaaa52ee87e0c7b8df
Ana Nikolova <ani.nik19@gmail.com>
2019-10-01 09:27:44 +0200

    delete post

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,9 +3,20 @@ import axios from "axios";
 export const FETCH_POSTS = "fetch_posts";
 export const CREATE_POST = "create_post";
 export const FETCH_POST = "fetch_post";
+export const DELETE_POST = "delete_post";
 
 const ROOT_URL = "http://reduxblog.herokuapp.com/api";
 const API_KEY = "?key=PAPERCLIPi123";
+
+export function deletePost(id, callback) {
+  const request = axios
+    .delete(`${ROOT_URL}/posts/${id}${API_KEY}`)
+    .then(() => callback());
+  return {
+    type: DELETE_POST,
+    payload: id
+  };
+}
 
 export function fetchPosts() {
   const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
@@ -15,7 +26,7 @@ export function fetchPosts() {
   };
 }
 
-export function createPosts(values, callback) {
+export function createPost(values, callback) {
   const request = axios
     .post(`${ROOT_URL}/posts${API_KEY}`, values)
     .then(() => callback());
